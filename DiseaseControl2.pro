b11b4a7f37968953fb69225c85725e32fc4acbf6
yangxianping <735196447@qq.com>
2017-03-12 22:47:58 +0800

    first create

---
+++ DiseaseControl2.pro
@@ -0,0 +1,26 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2017-03-12T11:10:15
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = DiseaseControl2
+TEMPLATE = app
+
+
+SOURCES += src/main.cpp\
+        src/mainwindow.cpp \
+    src/imageviewer.cpp \
+    src/imagemanager.cpp
+
+HEADERS  += src/mainwindow.h \
+    src/imageviewer.h \
+    src/imagemanager.h
+
+FORMS    += src/mainwindow.ui
+
+RESOURCES += resource/imgqrc.qrc
---
+++ src/mainwindow.h
@@ -0,0 +1,71 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "imagemanager.h"
+#include "QWidget"
+class QLabel;
+class QMenu;
+class QAction;
+class QPushButton;
+class QMdiArea;
+class ImageManager;
+class QToolBar;
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+protected slots:
+//    void closeEvent(QCloseEvent *event);
+//    void dragEnterEvent(QDragEnterEvent * event);
+//    void dropEvent(QDropEvent * event);
+//    void keyPressEvent(QKeyEvent *event);
+//    void resizeEvent(QResizeEvent *event);
+
+//    bool eventFilter(QObject *obj, QEvent *event);
+//    void applyConfig();
+//    void imageChanged(const QString &fileName = QString::null);
+//    void showContextMenu(const QPoint &pos);
+
+//    void openFile();
+//    void changeFullScreen();
+//    void showAttribute();
+//    void switchSlideShow();
+//    void setting();
+//    void about();
+private:
+//    void readSettings();
+//    void writeSettings();
+
+    void createActions();
+    void createMenus();
+    void createToolBars();
+    void createStatusBar();
+    void createContentMenu();
+
+    void registerAllFunction();
+
+    Ui::MainWindow *ui;
+    ImageManager *viewer;
+    QRect attributeRect;
+    QMenu *fileMenu;
+    QToolBar *fileToolBar;
+    QMdiArea *mdiArea;
+    QLabel *readyLabel;
+
+    QAction *openAction;
+    QAction *saveAction;
+    QAction *saveAsAction;
+    QAction *exitAction;
+    QAction *newAction;
+};
+
+#endif // MAINWINDOW_H
